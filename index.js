606e732e6a58b0c20d8320e1378cdce0530f2bae
Ryan <ryanbiy01@gmail.com>
2023-03-14 21:43:46 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  function returnsANamedFunction(){
+  return function divide(){
+  
+  }
+  }
+  function  returnsAnAnonymousFunction(){
+      return function(){
+  
+      }
+  }
